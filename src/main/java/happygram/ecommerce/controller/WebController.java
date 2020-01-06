a4374a1151771fb4b5df7511e9d9f5ec7c7a03cd
happygram <jsacc0619@gmail.com>
2020-01-06 22:20:37 +0900

    동적으로 index 페이지에서 content 변경할 수 있도록 Controller 메소드 작성

--- src/main/java/happygram/ecommerce/controller/WebController.java
+++ src/main/java/happygram/ecommerce/controller/WebController.java
@@ -1,13 +1,20 @@
 package happygram.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
    @RequestMapping(value = "/")
-   public String index() {
-      System.out.println("test");
+   public String main(Model model) {
+      model.addAttribute("template", "fragments/content/main");
+      return "index";
+   }
+
+   @RequestMapping(value = "/product")
+   public String index(Model model) {
+      model.addAttribute("template", "fragments/content/product");
       return "index";
    }
 }
